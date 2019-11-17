5dfe8b44e89848b0f83797393ab329686a480be0
ytyaru <ytyaru@outlook.jp>
2019-11-17 12:05:37 +0900

    初回コミット

---
+++ src/GitHubAction_DotNetCommand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubAction_DotNetCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
